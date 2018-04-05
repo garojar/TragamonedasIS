51db178b8f4d830915d5840ca4ae2cc447e288bc
unknown <garojar@hotmail.com>
2018-04-05 13:10:51 -0300

    added lombok builder, getter and setter a Jugador

--- src/main/java/Jugador.java
+++ src/main/java/Jugador.java
@@ -1,3 +1,7 @@
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Clase que representa al jugador involucrado
  * en el Tragamonedas
@@ -5,17 +9,25 @@
  * @version 1.0
  *
  */
+@Builder
 public class Jugador {
     /**
      * Saldo disponible del jugador en el momento
      */
+    @Getter
+    @Setter
     private int saldoDisponible;
 
 
     /**
      * Monto que representa la apuesta del jugador
      */
+    @Getter
+    @Setter
     private int apuesta;
+
+    
+
 
 
 
